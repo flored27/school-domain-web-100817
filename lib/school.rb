68e4f2db41f8177cf4728dae79d7aef8129d4479
flored27 <dflores@fandm.edu>
2017-10-11 14:00:04 -0400

    done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,39 @@
-# code here!
+
+class School
+
+  require 'pry'
+  def initialize(schoolname)
+    @schoolname = schoolname
+    @roster = {}
+  end
+
+attr_accessor :roster, :schoolname
+
+
+
+  def add_student(name, grade)
+    namess = name
+    gradess = grade
+    if @roster == nil
+        @roster = {grade => []}
+        @roster[grade] << name
+    elsif @roster.keys.include?(grade)
+        @roster[grade] << name
+      elsif @roster.keys.include?(grade) == false
+        @roster[grade] = []
+        @roster[grade] << name
+      end
+  end
+
+
+  def grade(grade)
+    return roster[grade]
+  end
+
+  def sort
+
+    roster.each {|k, v| v.sort!}
+
+end
+
+end
